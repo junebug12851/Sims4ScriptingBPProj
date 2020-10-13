ebbfebb975a5fbdf09a92cce4bc8a9390aed2744
June Hanabi <junehanabi@gmail.com>
2020-10-13 13:05:19 -0400

    Added a starter script file and removed .gitkeep

---
+++ src/main.py
@@ -0,0 +1,24 @@
+#    Copyright 2020 June Hanabi
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import sims4.commands
+
+# This is just a Hello World script, you can play around with it, do a test compile/decompile/debug, or just delete it
+# and start from scratch. The world is yours.
+
+
+@sims4.commands.Command('hellow', command_type=sims4.commands.CommandType.Live)
+def _hellow(_connection=None):
+    output = sims4.commands.CheatOutput(_connection)
+    output("This is my first script mod")
